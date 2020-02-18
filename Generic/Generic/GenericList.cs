f7c2b7262943ae21d4a6d0049d925a95cc6b5538
Serena Bailey <47674941+BaileySerena@users.noreply.github.com>
2020-02-18 12:32:42 -0600

    Add files via upload
    Generic

---
+++ Generic/Generic/GenericList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generic
+{
+    class GenericList<T>
+    {
+        private T[] array;
+
+        public GenericList(int size)
+        {
+            array = new T[size + 1];
+        }
+        public T GetItem(int index)
+        {
+            return array[index];
+        }
+        public void SetItem(int index, T value)
+        {
+            array[index] = value;
+        }
+    }
+}
